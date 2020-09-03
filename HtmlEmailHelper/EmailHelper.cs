1d52a06dd9447d6304ada491ca4fb27140066649
Muhammad Mujahid <mmujahid@digital-dividend.com>
2020-09-03 17:09:16 +0500

    Add project files.

---
+++ HtmlEmailHelper/EmailHelper.cs
@@ -0,0 +1,70 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HtmlEmailHelper
+{
+    public class EmailFields
+    {
+        public EmailFields() { }
+        public EmailFields(string subject, string to)
+        {
+            this.Subject = subject;
+            this.To = to;
+        }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public EmailTemplate Template { get; set; }
+        public EmailTemplateFields TemplateFields { get; set; }
+    }
+    public class EmailTemplateFields
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public static EmailTemplateFields ExampleTemplate(string name)
+        {
+            return new EmailTemplateFields()
+            {
+                Name = name
+            };
+        }
+    }
+    public class SendGridOptions
+    {
+        public string ApiKey { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public enum EmailTemplate
+    {
+        ExampleTemplate
+    }
+    public static class EmailHelper
+    {
+        public static string TemplateString(EmailTemplate template, EmailTemplateFields fields)
+        {
+            return System.IO.File.ReadAllText(@$"EmailTemplates\{template}.html")
+                .Replace($"[{nameof(fields.Name)}]", fields.Name);
+        }
+        private static Response Send(EmailFields emailFields, SendGridOptions sendGridOptions)
+        {
+            var client = new SendGridClient(sendGridOptions.ApiKey);
+            var msg = MailHelper.CreateSingleEmail(
+                new EmailAddress(sendGridOptions.Email, sendGridOptions.Name),
+                new EmailAddress(emailFields.To, string.Empty),
+                emailFields.Subject,
+                string.Empty,
+                TemplateString(emailFields.Template, emailFields.TemplateFields)
+                );
+            return client.SendEmailAsync(msg).Result;
+        }
+
+        public static Response SendEmail(this EmailFields emailFields, SendGridOptions sendGridOptions)
+        {
+            return Send(emailFields, sendGridOptions);
+        }
+    }
+}
---
+++ HtmlEmailHelper/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HtmlEmailHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Veriables
+            string email = string.Empty;
+            string name = string.Empty;
+            SendGridOptions sendGrid = new SendGridOptions()
+            {
+                ApiKey = "YOUR SENDGRID API KEY",
+                Email = "YOUR FROM EMAIL",
+                Name = "YOUR COMPANY OR PERSONAL EMAIL"
+            };
+
+            //Read Email
+            do
+            {
+                Console.WriteLine("Enter Your Email:");
+                email = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(email));
+
+            //Read Name
+            do
+            {
+                Console.WriteLine("Enter Your Name:");
+                name = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(name));
+
+            //Initialize template email
+            var emailFields = new EmailFields("email subject", email);
+            emailFields.Template = EmailTemplate.ExampleTemplate;
+            emailFields.TemplateFields = EmailTemplateFields.ExampleTemplate(name);
+
+            try
+            {
+                //Send Email
+                emailFields.SendEmail(sendGrid);
+            }
+            catch (Exception ex)
+            {
+                //Catch Error
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
