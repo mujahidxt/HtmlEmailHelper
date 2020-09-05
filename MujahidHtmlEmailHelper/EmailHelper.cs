477a3087991a8dc0954e3b9ec0dd60b106784b08
Muhammad Mujahid <mmujahid@digital-dividend.com>
2020-09-05 21:55:12 +0500

    just removed extra ns

--- MujahidHtmlEmailHelper/EmailHelper.cs
+++ MujahidHtmlEmailHelper/EmailHelper.cs
@@ -1,10 +1,7 @@
 ﻿using SendGrid;
 using SendGrid.Helpers.Mail;
-using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Mail;
-using System.Text;
 
 namespace MujahidHtmlEmailHelper
 {
