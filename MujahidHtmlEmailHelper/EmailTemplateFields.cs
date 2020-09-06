7aad4dc7d664d4ec77402794fbd679d1d0df8048
Muhammad Mujahid <mmujahid@digital-dividend.com>
2020-09-06 15:00:39 +0500

    v1.0.5

--- MujahidHtmlEmailHelper/EmailTemplateFields.cs
+++ MujahidHtmlEmailHelper/EmailTemplateFields.cs
@@ -5,17 +5,19 @@ namespace MujahidHtmlEmailHelper
     public class EmailTemplateFields : IEmailTemplateFields
     {
         public EmailTemplateFields() { }
-        public EmailTemplateFields(string template, Dictionary<string, string> fields)
+        public EmailTemplateFields(string template, Dictionary<string, string> fields, TemplateType templateType = TemplateType.HtmlFile)
         {
             this.Template = template;
+            this.TemplateType = templateType;
             this.Fields = fields;
         }
         public string Template { get; set; }
+        public TemplateType TemplateType { get; set; }
         public Dictionary<string, string> Fields { get; set; }
 
-        public static IEmailTemplateFields EmailTemplate(string template, Dictionary<string, string> fields)
+        public static IEmailTemplateFields EmailTemplate(string template, Dictionary<string, string> fields,TemplateType templateType=TemplateType.HtmlFile)
         {
-            return new EmailTemplateFields(template, fields);
+            return new EmailTemplateFields(template, fields, templateType);
         }
     }
 }
--- MujahidHtmlEmailHelper/IEmailTemplateFields.cs
+++ MujahidHtmlEmailHelper/IEmailTemplateFields.cs
@@ -5,6 +5,7 @@ namespace MujahidHtmlEmailHelper
     public interface IEmailTemplateFields
     {
         string Template { get; set; }
+        TemplateType TemplateType { get; set; }
         Dictionary<string, string> Fields { get; set; }
     }
 }
